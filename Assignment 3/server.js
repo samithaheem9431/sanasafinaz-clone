d21c9b1292eec834f8fe2f4aa19da9db518ceaf5
Abdul Sami <abdulsamikhan471@gmail.com>
2024-11-18 22:00:39 +0500

    Files Added

--- Assignment 3/server.js
+++ Assignment 3/server.js
@@ -13,45 +13,6 @@ app.get("/",(req,res)=>{
     res.render("home");
 })
 
-app.listen(3000,() => {
+app.listen(5000,() => {
     console.log("Server started at location: 5000")
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const express = require("express");
-// let app  = express();
-
-// app.get("/contact-us",(req,res)=>{
-//     res.send("<h1> Hello Section B Contact</h1>")
-// })
-
-// app.get("/",(req,res)=>{
-//     res.send("<h1> Hello Section A Home</h1>")
-// })
-
-// app.get("/update",(req,res)=>{
-//     res.send("<h1> Hello Section update</h1>")
-// })
-// app.get("/create",(req,res)=>{
-//     res.send("<h1> Hello create</h1>")
-// })
-
-// app.listen(5001,() => {
-//     console.log("Server started at location: 5001")
-// })
